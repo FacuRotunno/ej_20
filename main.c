5393a3e0e564d733a8c31c3b410434f5cdf83295
Facundo Rotunno <facu.rotunno@gmail.com>
2019-05-10 20:10:28 -0300

    Presupuestos

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+
+{
+
+    float monto;
+    float mu=0 ;
+    float mp=0 ;
+    float mt=0;
+
+    printf ("Ingresar presupuesto anual:$");
+    scanf ("%f",&monto);
+
+
+
+
+    printf("Presupuesto anual de Urgencias:$%f  \nPresupuesto anual de Traumatologia:$%f \nPresupúesto anual de Pediatria:$%f ", mu = monto / 37, mt = monto/21, mp= monto / 42);
+}
